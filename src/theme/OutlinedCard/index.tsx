1ea3ae419ca92b0852fe7dc4872999bfdaea3bab
Ad-Bean <adbeanx@outlook.com>
2023-07-12 15:19:38 +0800

    fix: card link color

--- src/theme/OutlinedCard/index.tsx
+++ src/theme/OutlinedCard/index.tsx
@@ -110,7 +110,7 @@ export default function OutlinedCard({
                     {link.text}
                     {link.url && <ExternalArrow />}
                     {link.doc && (
-                      <RightArrow fill={dark ? "#48dcbc" : "#0098ef"} />
+                      <RightArrow fill={dark ? "#0098ef" : "#48dcbc"} />
                     )}
                   </Typography>
                 </div>
@@ -159,10 +159,9 @@ const ExternalArrow = () => (
     xmlns="http://www.w3.org/2000/svg"
     className={styles.externalArrowIcon}
   >
-    <path d="M15 3.5H20.5M20.5 3.5V9M20.5 3.5L12.5 11.5" stroke="#2f5dd9" />
+    <path d="M15 3.5H20.5M20.5 3.5V9M20.5 3.5L12.5 11.5" />
     <path
       d="M11.5 5.5H7.5C6.39543 5.5 5.5 6.39543 5.5 7.5V16.5C5.5 17.6046 6.39543 18.5 7.5 18.5H16.5C17.6046 18.5 18.5 17.6046 18.5 16.5V12.5"
-      stroke="#2f5dd9"
       strokeLinecap="round"
     />
   </svg>
