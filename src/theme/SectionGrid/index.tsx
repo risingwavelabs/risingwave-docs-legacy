ca70f6025b897870c74cad0372ec21bac24183e8
Ad-Bean <adbeanx@outlook.com>
2023-07-20 15:02:56 +0800

    feat: cols

--- src/theme/SectionGrid/index.tsx
+++ src/theme/SectionGrid/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 type Icons = {
   name: string;
@@ -8,18 +9,25 @@ type Icons = {
 };
 
 type Props = {
+  cols: number;
   children: React.ReactNode;
   icons: Icons[];
 };
 
-function SectionGrid({ children, icons }: Props) {
+function SectionGrid({ children, icons, cols }: Props) {
+  const matches = useMediaQuery("(min-width:640px)");
   return (
     <section>
       <div className={styles.sectionContainer}>
         <div className={styles.gridContainer}>
           <div className={styles.leftContainer}>{children}</div>
 
-          <div className={styles.rightContainer}>
+          <div
+            className={styles.rightContainer}
+            style={{
+              gridTemplateColumns: matches ? `repeat(${cols ?? 3}, minmax(0, 1fr))` : "repeat(2, minmax(0, 1fr))",
+            }}
+          >
             {icons?.map((icon) => (
               <a key={icon.name} className={styles.iconContainer} href={icon.href}>
                 <img alt={icon.name} src={icon.src} className={`${styles.icon} disabled-zoom`} />
