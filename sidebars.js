26c298003343726173cd25d5d0428333c03e169f
hengm3467 <100685635+hengm3467@users.noreply.github.com>
2023-08-08 16:59:05 +0800

    add troubleshoot and performance tuning containers

--- sidebars.js
+++ sidebars.js
@@ -676,6 +676,16 @@ const sidebars = {
     },
     {
       type: "doc",
+      label: "Optimize performance",
+      id: "performance/optimize-performance"
+    },
+    {
+      type: "doc",
+      label: "Troubleshooting",
+      id: "troubleshoot/troubleshooting"
+    },
+    {
+      type: "doc",
       label: "Releases",
       id: "release-notes",
     },
