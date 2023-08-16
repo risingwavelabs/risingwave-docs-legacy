a09aca98826e4039c9169023f6001f1165032cc6
Ad-Bean <adbeanx@outlook.com>
2023-08-16 15:57:20 +0800

    fix: promise

--- algolia/index.js
+++ algolia/index.js
@@ -1,33 +1,30 @@
 const core = require("@actions/core");
 const fetch = require("node-fetch");
 
-try {
-  const CRAWLER_ID = core.getInput("CRAWLER_ID");
-  const CRAWLER_USER_ID = core.getInput("CRAWLER_USER_ID");
-  const CRAWLER_API_KEY = core.getInput("CRAWLER_API_KEY");
-  const CRAWLER_API_BASE_URL = core.getInput("CRAWLER_API_BASE_URL");
+const CRAWLER_ID = core.getInput("CRAWLER_ID");
+const CRAWLER_USER_ID = core.getInput("CRAWLER_USER_ID");
+const CRAWLER_API_KEY = core.getInput("CRAWLER_API_KEY");
+const CRAWLER_API_BASE_URL = core.getInput("CRAWLER_API_BASE_URL");
 
-  const BASE64_BASIC_AUTH = `Basic ${Buffer.from(`${CRAWLER_USER_ID}:${CRAWLER_API_KEY}`).toString("base64")}`;
+const BASE64_BASIC_AUTH = `Basic ${Buffer.from(`${CRAWLER_USER_ID}:${CRAWLER_API_KEY}`).toString("base64")}`;
 
-  async function reindex(crawlerId) {
-    console.log(`
+async function reindex(crawlerId) {
+  console.log(`
     ${new Date().toTimeString()}:
     Triggering reindex on ${crawlerId}
     `);
 
-    const res = await fetch(`${CRAWLER_API_BASE_URL}/crawlers/${crawlerId}/reindex`, {
-      method: "POST",
-      headers: {
-        Authorization: BASE64_BASIC_AUTH,
-        "Content-Type": "application/json",
-      },
-    });
-    const jsonResponse = await res.json();
-    return jsonResponse;
-  }
-
-  const res = await reindex(CRAWLER_ID);
-  console.log(res);
-} catch (error) {
-  core.setFailed(error.message);
+  const res = await fetch(`${CRAWLER_API_BASE_URL}/crawlers/${crawlerId}/reindex`, {
+    method: "POST",
+    headers: {
+      Authorization: BASE64_BASIC_AUTH,
+      "Content-Type": "application/json",
+    },
+  });
+  const jsonResponse = await res.json();
+  return jsonResponse;
 }
+
+reindex(CRAWLER_ID)
+  .then((res) => console.log(res))
+  .catch((err) => core.setFailed(err));
