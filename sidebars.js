b471683ab69a48ca7922b8fb739d8fe4292b5dcb
WanYixian <norawanofficial@gmail.com>
2024-04-08 17:12:17 +0800

    adjust the sidebar

--- sidebars.js
+++ sidebars.js
@@ -251,11 +251,6 @@ const sidebars = {
                       id: "ingest/ingest-from-instaclustr-kafka",
                     },
                     {
-                      "type": "doc",
-                      "label": "RedHat AMQ Streams",
-                      "id": "ingest/ingest-from-redhat-amq-streams"
-                    },
-                    {
                       type: "doc",
                       label: "Upstash Kafka",
                       id: "ingest/ingest-from-upstash-kafka",
@@ -352,6 +347,11 @@ const sidebars = {
                   type: "doc",
                   label: "NATS JetStream",
                   id: "ingest/ingest-from-nats",
+                },
+                {
+                  "type": "doc",
+                  "label": "RedHat AMQ Streams",
+                  "id": "ingest/ingest-from-redhat-amq-streams"
                 },
                 {
                   type: "doc",
