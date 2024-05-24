1272e6a8a6081e5e49f5b3b09df76553e243031e
Tianchun Luo <luotianchun123@qq.com>
2024-05-24 15:35:41 +0800

    fix: building

--- src/theme/DocPage/Layout/Main/index.js
+++ src/theme/DocPage/Layout/Main/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import {useDocsSidebar} from '@docusaurus/theme-common/internal';
 import styles from './styles.module.css';
 export default function DocPageLayoutMain({hiddenSidebarContainer, children}) {
+  const [iscloud,setIscloud] = useState(false)
   const sidebar = useDocsSidebar();
+  useEffect(()=>{
+    if(window?.location?.pathname?.includes?.('cloud')){
+      setIscloud(true)
+    }
+  },[])
   return (
     <main
       className={clsx(
         styles.docMainContainer,
         (hiddenSidebarContainer || !sidebar) && styles.docMainContainerEnhanced,
       )}>
-        {location?.pathname?.includes?.('cloud') ? <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c" /> :
+        {iscloud ? <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c" /> :
         <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=2e45a652-ff43-4a5a-a189-3c27f799428c" />
         }
       <div
