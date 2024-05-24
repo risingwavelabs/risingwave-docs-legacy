79a449863009d9343f1c046b2461611e55de7eb1
Tianchun Luo <luotianchun123@qq.com>
2024-05-24 15:52:33 +0800

    feat: use uselocation

--- src/theme/DocPage/Layout/Main/index.js
+++ src/theme/DocPage/Layout/Main/index.js
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import {useDocsSidebar} from '@docusaurus/theme-common/internal';
+import { useLocation } from "@docusaurus/router";
 import styles from './styles.module.css';
+
 export default function DocPageLayoutMain({hiddenSidebarContainer, children}) {
-  const [iscloud,setIscloud] = useState(false)
   const sidebar = useDocsSidebar();
-  useEffect(()=>{
-    if(window?.location?.pathname?.includes?.('cloud')){
-      setIscloud(true)
-    }
-  },[])
+  const location = useLocation();
   return (
     <main
       className={clsx(
         styles.docMainContainer,
         (hiddenSidebarContainer || !sidebar) && styles.docMainContainerEnhanced,
       )}>
-        {iscloud ? <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c" /> :
+        {location?.pathname?.includes?.('cloud') ? <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c" /> :
         <img referrerpolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=2e45a652-ff43-4a5a-a189-3c27f799428c" />
         }
       <div
