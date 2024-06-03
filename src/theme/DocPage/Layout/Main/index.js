7ee4a3ef3f281ca189a200a191d3ff9e126f2aa5
Ad-Bean <adbeanx@outlook.com>
2024-06-03 14:52:05 +0800

    fix: move under main

--- src/theme/DocPage/Layout/Main/index.js
+++ src/theme/DocPage/Layout/Main/index.js
@@ -11,19 +11,6 @@ export default function DocPageLayoutMain({ hiddenSidebarContainer, children }) 
     <main
       className={clsx(styles.docMainContainer, (hiddenSidebarContainer || !sidebar) && styles.docMainContainerEnhanced)}
     >
-      {location?.pathname?.includes?.("cloud") ? (
-        <img
-          referrerpolicy="no-referrer-when-downgrade"
-          className={styles.scarfHidden}
-          src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c"
-        />
-      ) : (
-        <img
-          referrerpolicy="no-referrer-when-downgrade"
-          className={styles.scarfHidden}
-          src="https://static.scarf.sh/a.png?x-pxid=2e45a652-ff43-4a5a-a189-3c27f799428c"
-        />
-      )}
       <div
         className={clsx(
           "container padding-top--md padding-bottom--lg",
@@ -31,6 +18,17 @@ export default function DocPageLayoutMain({ hiddenSidebarContainer, children }) 
           hiddenSidebarContainer && styles.docItemWrapperEnhanced
         )}
       >
+        {location?.pathname?.includes?.("cloud") ? (
+          <img
+            referrerpolicy="no-referrer-when-downgrade"
+            src="https://static.scarf.sh/a.png?x-pxid=63a86d76-f62d-4852-9f08-79a13d42ec3c"
+          />
+        ) : (
+          <img
+            referrerpolicy="no-referrer-when-downgrade"
+            src="https://static.scarf.sh/a.png?x-pxid=2e45a652-ff43-4a5a-a189-3c27f799428c"
+          />
+        )}
         {children}
       </div>
     </main>
