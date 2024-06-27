a293bf2419d64ce528e2f225d1ef1f1bcb727edc
WanYixian <150207222+WanYixian@users.noreply.github.com>
2024-06-27 16:09:14 +0800

    resolve conflict

--- sidebarCloud.js
+++ sidebarCloud.js
@@ -65,7 +65,7 @@ module.exports = {
             },
             {
               type: "doc",
-              id: "project-connection-errors",
+              id: "cluster-connection-errors",
             },
             {
               type: "doc",
